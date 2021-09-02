55dc8588ce60cfefb5b74282ed8b70b295e06d91
Joel Stella <stella.joelivan@gmail.com>
2021-09-02 17:05:05 -0300

    DB config

--- src/main/java/com/distribuidos/rcp/GRPCServer.java
+++ src/main/java/com/distribuidos/rcp/GRPCServer.java
@@ -1,13 +1,19 @@
 package com.distribuidos.rcp;
 
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
-import java.io.IOException;
-
+@SpringBootApplication
 public class GRPCServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+    	SpringApplication.run(GRPCServer.class, args);
+    	
         Server server = ServerBuilder.forPort(9090).addService(new FarmaciaService()).build();
 
         server.start();
